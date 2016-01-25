0c1fb13c6895277ca4db56b06e0962c2c52607a3
Stephen Pratt <stevefsp@gmail.com>
2016-01-25 16:25:31 -0700

    Core: Add ObjectList Feature
    ObjectList: A list of Unity Objects that must be of a give type.  Useful
    for serialization of an array of objects that are known to be Unity
    Objects. but present a non-Unity Object interface during normal use.
    E.g. A list of Unity Objects that all implement the IAgent interface.
    
    ObjectListDrawer: A GUI element representing a reorderable list of
    single-line object references.  Provides a user friendly GUI element for
    ObjectList implementations.

---
+++ Source/Plugins/Lizitt/Utils/Core/Editor/ObjectListDrawer.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * Copyright (c) 2016 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEditor;
+using UnityEngine;
+using com.lizitt.editor;
+
+namespace com.lizitt.editor
+{
+    /// <summary>
+    /// A GUI element representing a reorderable list of single-line object references.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The object refrence elements are drawn on a single line without labels. Custom
+    /// multi-line list elements are not supported.
+    /// </para>
+    /// <para>
+    /// This drawer supports two scenarios: The default behavior is that the list property is 
+    /// the same one provided to <see cref="OnGUI"/>.  But it can also be an arbitrary sub-property.
+    /// Concrete classes can override <see cref="GetListProperty"/> to override the defualt
+    /// behavior.
+    /// </para>
+    /// <para>
+    /// This object list is expecially useful for lists the need to be restricted but can't
+    /// be restricted using Unity's standard object picker.  For example, you may want to 
+    /// only accepted object's that implement a specific interface. Override <see cref="Validate"/>
+    /// to implement custom validations.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="ReferenceListComponent"/>
+    public abstract class ObjectListDrawer
+        : PropertyDrawer
+    {
+        ReorderableListComponent m_GuiElement;
+
+        /// <summary>
+        /// See Unity documentation.
+        /// </summary>
+        /// <param name="property">See Unity documentation.</param>
+        /// <param name="label">See Unity documentation.</param>
+        /// <returns>See Unity documentation.</returns>
+        public sealed override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var listProp = GetListProperty(property);
+
+            CheckInitialized(listProp);
+
+            return m_GuiElement.GetPropertyHeight(listProp);
+        }
+
+        /// <summary>
+        /// See Unity documentation.
+        /// </summary>
+        /// <param name="position">See Unity documentation.</param>
+        /// <param name="property">See Unity documentation.</param>
+        /// <param name="label">See Unity documentation.</param>
+        public sealed override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var listProp = GetListProperty(property);
+
+            CheckInitialized(listProp);
+
+            m_GuiElement.OnGUI(position, listProp, label);
+        }
+
+        private void CheckInitialized(SerializedProperty listProperty)
+        {
+            if (m_GuiElement == null)
+            {
+                m_GuiElement = new ReorderableListComponent(listProperty, HeaderTitle, true,
+                    EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing * 3, 
+                    DrawElement, HandleAddElement);
+            }
+        }
+
+        private void HandleAddElement(SerializedProperty elementProperty)
+        {
+            elementProperty.objectReferenceValue = null;
+        }
+
+        private void DrawElement(Rect position, SerializedProperty elementProperty,
+            bool isActive, bool isFocused)
+        {
+            position = EditorGUIUtil.SingleLinePosition(
+                position, EditorGUIUtility.standardVerticalSpacing);
+
+            EditorGUI.PropertyField(position, elementProperty, GUIContent.none);
+
+            var comp = elementProperty.objectReferenceValue;
+            if (comp && !ValidateComponent(comp))
+                elementProperty.objectReferenceValue = null;
+        }
+
+        /// <summary>
+        /// The title to be displayed in the list's header.
+        /// </summary>
+        protected abstract string HeaderTitle { get; }
+
+        /// <summary>
+        /// The list property being drawn.
+        /// </summary>
+        /// <param name="property">
+        /// The property from <see cref="OnGUI"/>  and <see cref="GetPropertyHeight"/>.
+        /// </param>
+        /// <returns>The list property being drawn.</returns>
+        protected virtual SerializedProperty GetListProperty(SerializedProperty property)
+        {
+            return property;
+        }
+
+        /// <summary>
+        /// Test the reference object for validity, reporting any errors.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This method is called during <see cref="OnGUI"/> to validate assignments to each
+        /// list element. If an assignment is invalid, the element will be set to null.
+        /// </para>
+        /// </remarks>
+        /// <param name="obj">The object being validated.</param>
+        /// <returns>True if the object is valid, otherwise false.</returns>
+        protected virtual bool ValidateComponent(Object obj)
+        {
+            return true;
+        }
+    }
+}
+
---
+++ Source/Plugins/Lizitt/Utils/Core/ObjectList.cs
@@ -0,0 +1,206 @@
+﻿/*
+ * Copyright (c) 2016 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace com.lizitt
+{
+    /// <summary>
+    /// A list of Unity Objects that must be of a give type.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Does not support the addition/assignment of null or duplicate entries.  But the list may
+    /// contain null entries if objects are destroyed while in the list.
+    /// </para>
+    /// <para>
+    /// This class is useful for serialization of objects that are known to be Unity objects
+    /// but present a non-Unity object interface during normal use.  E.g. A list of Unity objects
+    /// that all implement the IAgent interface.
+    /// </para>
+    /// <para>
+    /// Use in conjunction with the <see cref="ObjectListDrawer"/> to present a user friendly
+    /// GUI element for object lists.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="T">The required type.</typeparam>
+    public abstract class ObjectList<T>
+        where T : class
+    {
+        [SerializeField]
+        private List<Object> m_Items;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="bufferSize">The initial size of the object buffer.</param>
+        public ObjectList(int bufferSize)
+        {
+            m_Items = new List<Object>(Mathf.Max(0, bufferSize));
+        }
+
+        /// <summary>
+        /// The current size of the object buffer.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Because Unity objects can be destroyed, objects added to this list may become
+        /// null after they are added.
+        /// </para>
+        /// </remarks>
+        public int BufferSize
+        {
+            get { return m_Items.Count; }
+        }
+
+        /// <summary>
+        /// The item at the specified index, or null.
+        /// </summary>
+        /// <param name="index">
+        /// Item index. [Limit: 0 &lt;= value &lt; <see cref="BufferSize"/>]
+        /// </param>
+        /// <returns>The item at the specified index, or null.</returns>
+        public T this[int index]
+        {
+            get { return GetItem(index); }
+            set { SetItem(index, value); }
+        }
+
+        /// <summary>
+        /// Get the item as the specified index, or null if there is no valid reference.
+        /// (E.g. The object was destroyed.)
+        /// </summary>
+        /// <param name="index">The item index.</param>
+        /// <returns>The item as the specified index, or null if there is no valid reference.
+        /// (E.g. The object was destroyed.)</returns>
+        public T GetItem(int index)
+        {
+            return m_Items[index] ? m_Items[index] as T : null;
+        }
+
+        /// <summary>
+        /// Set the value at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="value">A non-null Unity Object. (Required)</param>
+        /// <param name="refObj">
+        /// An object reference to use for more friendly error reporting. (Optional)
+        /// </param>
+        public void SetItem(int index, T value, Object refObj = null)
+        {
+            var obj = value as Object;
+
+            if (!obj)
+            {
+                Debug.LogError("Item is null or not a Unity Object. Assignment ignored.", refObj);
+                return;
+            }
+
+            m_Items[index] = obj;
+        }
+
+        /// <summary>
+        /// Add an item to the list.  
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Duplicates are not allowed.  If an attempt to add a duplicate is detected, its
+        /// current index location will be returned.
+        /// </para>
+        /// </remarks>
+        /// <param name="item">A non-null Unity Object. (Required)</param>
+        /// <param name="refObj">
+        /// An object reference to use for more friendly error reporting. (Optional)
+        /// </param>
+        /// <returns>The index where the items was stored, or -1 if the item was rejected.</returns>
+        public int Add(T item, Object refObj = null)
+        {
+            var obj = item as Object;
+            if (!obj)
+            {
+                Debug.LogError(typeof(T).Name + " item is not a Unity Object: " + item, refObj);
+                return -1;
+            }
+
+            var i = m_Items.IndexOf(obj);
+            if (i != -1)
+                return i;
+
+            m_Items.Add(obj);
+            return m_Items.Count - 1;
+        }
+
+        /// <summary>
+        /// Remove the specifed item from the list.
+        /// </summary>
+        /// <param name="item">The item to remove.</param>
+        /// <returns>True if the item was found in the list and removed.</returns>
+        public bool Remove(T item)
+        {
+            var obj = item as Object;
+            if (!obj)
+                return false;
+
+            return m_Items.Remove(obj);
+        }
+
+        /// <summary>
+        /// Remove all items from the list.
+        /// </summary>
+        public void Clear()
+        {
+            m_Items.Clear();
+        }
+
+        /// <summary>
+        /// Remove all null items from the list.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// While null items can't be added to the list, objects can be destroyed by Unity, causing
+        /// them to become effectively null.
+        /// </para>
+        /// </remarks>
+        public void PurgeNulls()
+        {
+            for (int i = m_Items.Count - 1; i >= 0; i--)
+            {
+                if (!m_Items[i])
+                    m_Items.RemoveAt(i);
+            }
+        }
+
+        /// <summary>
+        /// True if the list contains the specified object.
+        /// </summary>
+        /// <param name="item">The object.</param>
+        /// <returns>True if the list contains the specified object.</returns>
+        public bool Contains(T item)
+        {
+            var obj = item as Object;
+            if (!obj)
+                return false;
+
+            return m_Items.Contains(obj);
+        }
+    }
+}
